2f203188b796cbd25f0c1840a85ae5987083d9a8
Nora <nb@fournova.com>
2019-02-15 22:09:09 +0100

    Add exercises for Variables chapter

---
+++ Variables/name.rb
@@ -0,0 +1,21 @@
+#Exercise 1
+
+puts "Please enter your name:"
+
+name = gets.chomp
+
+puts "Welcome, #{name}!"
+
+#Exercise 3
+
+10.times { puts name }
+
+#Exercise 4
+
+puts "Please enter your first name:"
+firstname = gets.chomp
+
+puts "Please enter your last name:"
+lastname = gets.chomp
+
+puts "How are you, #{firstname} #{lastname}?"
